f1e7f1a35e29a196371a90fa84ed323e125e4ecd
Yoga_Ubuntu24 <11301998@qq.com>
2024-10-06 07:37:54 +0800

    增加注释 	modified:   src/WdInclude.h

--- src/WdInclude.h
+++ src/WdInclude.h
@@ -3,56 +3,57 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ElegantOTA.h>
-
+//define WelldoneESP32 specific build flags here
+//define Serial baud rate,not defined default is 115200
 #ifndef SerialBaudRate
 #define SerialBaudRate 115200
-#endif // !SerialBaudRate
-
+#endif 
+//define use or not wifi,default is true
 #ifndef UseWifi
 #define UseWifi true
-#endif // !UseWifi
-
+#endif 
+//WifiMode, 1=WIFI_STA or 2=WIFI_AP,default is 1 WIFI_STA
 #ifndef WifiMode
 #define WifiMode 1
-#endif // !WifiMode
-
+#endif 
+//define default wifi ssid and password
 #ifndef StationSsid 
 #define StationSsid "Wifi_SSID"
-#endif // !StationSsid
+#endif 
 
 #ifndef StationPwd
 #define StationPwd "Wifi_PASSWD"
-#endif // !StationPwd
+#endif 
 
 #ifndef ApSsid
 #define ApSsid "WdEsp32"
-#endif // !ApSsid
+#endif 
 
- #ifndef ApPwd
- #define ApPwd ""
- #endif // !ApPwd
+#ifndef ApPwd
+#define ApPwd ""
+#endif 
 
- #ifndef WdHostName
- #define WdHostName "WdEsp32"
- #endif // !WdHostName
+#ifndef WdHostName
+#define WdHostName "WdEsp32"
+#endif 
+//define use or not tcp client,default is true
+#ifndef UseTcpClient
+#define UseTcpClient true
+#endif 
 
- #ifndef UseTcpClient
- #define UseTcpClient true
- #endif // !UseTcpClient
+#ifndef RemoteIP
+#define RemoteIP "192.168.16.222"
+#endif 
 
- #ifndef RemoteIP
- #define RemoteIP "192.168.16.222"
- #endif // !RemoteIP
- 
- #ifndef RemotePort
- #define RemotePort 1234
- #endif // !RemotePort
- 
- #ifndef UseTcpServer
- #define UseTcpServer true
- #endif // !UseTcpServer
- 
- #ifndef LocalPort
- #define LocalPort 1234
- #endif // !LocalPort
+#ifndef RemotePort
+#define RemotePort 1234
+#endif 
+//define use or not tcp server,default is true
+#ifndef UseTcpServer
+#define UseTcpServer true
+#endif 
+
+#ifndef LocalPort
+#define LocalPort 5001
+#endif 
 #endif /* __WD_INCLUDE_H__ */
